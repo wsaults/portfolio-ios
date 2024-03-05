3dc3ae0c58ef2ff4f67c40da2f3b7ca3663e4349
Will Saults <will@saults.io>
2024-03-04 21:16:52 -0600

    Add CoreData DataController with Issue and Tag models

--- Portfolio/PortfolioApp.swift
+++ Portfolio/PortfolioApp.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 @main
 struct PortfolioApp: App {
+    @StateObject var dataController = DataController()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+                .environmentObject(dataController)
         }
     }
 }
