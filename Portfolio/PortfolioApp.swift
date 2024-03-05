a2e329b30fb41bda870cc91ae296859c7dabfb0c
Will saults <will@saults.io>
2024-03-04 20:25:32 -0600

    Initial Commit

---
+++ Portfolio/PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioApp.swift
+//  Portfolio
+//
+//  Created by Will Saults on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
