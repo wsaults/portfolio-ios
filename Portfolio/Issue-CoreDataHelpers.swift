f1fbacb2f9a2bca8039ad357b0a9c3afd242d142
Will Saults <will@saults.io>
2024-03-06 15:24:21 -0600

    Add Issue-CoreDataHelpers to abstract away nil-collessing operations

---
+++ Portfolio/Issue-CoreDataHelpers.swift
@@ -0,0 +1,40 @@
+//
+//  Issue-CoreDataHelpers.swift
+//  Portfolio
+//
+//  Created by Will Saults on 3/6/24.
+//
+
+import Foundation
+
+extension Issue {
+    var issueTitle: String {
+        get { title ?? "" }
+        set { title = newValue }
+    }
+    
+    var issueContent: String {
+        get { content ?? "" }
+        set { content = newValue }
+    }
+    
+    var issueCreationDate: Date {
+        creationDate ?? .now
+    }
+    
+    var issueModificationDate: Date {
+        modificationDate ?? .now
+    }
+    
+    static var example: Issue {
+        let controller = DataController(inMemory: true)
+        let viewContext = controller.container.viewContext
+        
+        let issue = Issue(context: viewContext)
+        issue.title = "Example Issue"
+        issue.content = "This is an example issue."
+        issue.priority = 2
+        issue.creationDate = .now
+        return issue
+    }
+}
