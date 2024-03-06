5a72d81cd38395f179b9d61561173bd364694d50
Will Saults <will@saults.io>
2024-03-06 15:50:20 -0600

    Add helpers to fetch Issues and Tags

--- Portfolio/Issue-CoreDataHelpers.swift
+++ Portfolio/Issue-CoreDataHelpers.swift
@@ -26,6 +26,11 @@ extension Issue {
         modificationDate ?? .now
     }
     
+    var issueTags: [Tag] {
+        let result = tags?.allObjects as? [Tag] ?? []
+        return result.sorted()
+    }
+    
     static var example: Issue {
         let controller = DataController(inMemory: true)
         let viewContext = controller.container.viewContext
@@ -36,5 +41,18 @@ extension Issue {
         issue.priority = 2
         issue.creationDate = .now
         return issue
+    }
+}
+
+extension Issue: Comparable {
+    public static func <(lhs: Issue, rhs: Issue) -> Bool {
+        let left = lhs.issueTitle.localizedLowercase
+        let right = rhs.issueTitle.localizedLowercase
+        
+        if left == right {
+            return lhs.issueCreationDate < rhs.issueCreationDate
+        } else {
+            return left < right
+        }
     }
 }
