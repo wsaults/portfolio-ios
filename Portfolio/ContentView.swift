9df45e50165562048295ff79e43e635d83e83619
Will Saults <will@saults.io>
2024-03-09 21:55:48 -0600

    Handle issue and Tag deletions

--- Portfolio/ContentView.swift
+++ Portfolio/ContentView.swift
@@ -30,8 +30,16 @@ struct ContentView: View {
             ForEach(issues) { issue in
                 IssueRow(issue: issue)
             }
+            .onDelete(perform: delete)
         }
         .navigationTitle("Issues")
+    }
+    
+    func delete(_ offsets: IndexSet) {
+        for offset in offsets {
+            let item = issues[offset]
+            dataController.delete(item)
+        }
     }
 }
 
