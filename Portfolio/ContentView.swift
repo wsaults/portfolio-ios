e29b497b7323dbeb585d26ca89816a29639652bc
Will Saults <will@saults.io>
2024-03-09 21:38:46 -0600

    Add and display IssueRows in ContentView

--- Portfolio/ContentView.swift
+++ Portfolio/ContentView.swift
@@ -8,8 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject var dataController: DataController
+    
+    var issues: [Issue] {
+        let filter = dataController.selectedFilter ?? .all
+        var allIssues: [Issue]
+        
+        if let tag = filter.tag {
+            allIssues = tag.issues?.allObjects as? [Issue] ?? []
+        } else {
+            let request = Issue.fetchRequest()
+            request.predicate = NSPredicate(format: "modificationDate > %@", filter.minModificationDate as NSDate)
+            allIssues = (try? dataController.container.viewContext.fetch(request)) ?? []
+        }
+        
+        return allIssues.sorted()
+    }
+    
     var body: some View {
-        Text("Content")
+        List {
+            ForEach(issues) { issue in
+                IssueRow(issue: issue)
+            }
+        }
+        .navigationTitle("Issues")
     }
 }
 
